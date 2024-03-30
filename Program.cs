7f6f0aed282da2cbc087db4b2eaf91e91f421cb1
Maria <mariya-ivanova@list.ru>
2024-03-30 14:35:51 +0200

    the solution is ready

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int GetNumber(string message){
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetNumber("Insert your number: ");
+int sum = 0;
+int temp = num;
+
+while (temp > 0) {
+    sum += temp % 10;
+    temp = temp / 10;
+}
+Console.WriteLine($"{num} -> {sum}");
